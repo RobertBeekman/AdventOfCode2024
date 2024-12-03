7cf969d0e851fb07ab3be3c99d0cc81ab5d974ea
Robert Beekman <robert.beekman@rr-wfm.com>
2024-12-03 09:45:23 +0100

    Day 2 and 3

---
+++ AdventOfCode2024/Day2/Program.cs
@@ -0,0 +1,81 @@
+﻿using Day2;
+
+void Part1(string[] input)
+{
+    var safeCount = 0;
+    foreach (var row in input)
+    {
+        var levels = row.Split(" ").Select(int.Parse).ToList();
+        var safe = Validate(levels);
+        if (safe)
+        {
+            safeCount++;
+        }
+    }
+
+    Console.WriteLine("Part 1: " + safeCount);
+}
+
+void Part2(string[] input)
+{
+    var safeCount = 0;
+    foreach (var row in input)
+    {
+        var levels = row.Split(" ").Select(int.Parse).ToList();
+        var safe = Validate(levels);
+        if (safe)
+        {
+            safeCount++;
+            continue;
+        }
+
+        // Bruteforce babyyyy
+        for (var index = 0; index < levels.Count; index++)
+        {
+            var dampened = new List<int>(levels);
+            dampened.RemoveAt(index);
+            if (Validate(dampened))
+            {
+                safeCount++;
+                break;
+            }
+        }
+    }
+
+    Console.WriteLine("Part 2: " + safeCount);
+}
+
+var input = await File.ReadAllLinesAsync("input1.txt");
+Part1(input);
+Part2(input);
+
+bool Validate(List<int> ints)
+{
+    var rowDirection = Direction.Undetermined;
+    var safe = true;
+    var previous = ints[0];
+    foreach (var level in ints.Skip(1))
+    {
+        var direction = level > previous ? Direction.Up : Direction.Down;
+        if (rowDirection == Direction.Undetermined)
+        {
+            rowDirection = direction;
+        }
+
+        if (direction != rowDirection)
+        {
+            safe = false;
+            break;
+        }
+
+        if (Math.Abs(previous - level) is 0 or > 3)
+        {
+            safe = false;
+            break;
+        }
+
+        previous = level;
+    }
+
+    return safe;
+}
---
+++ AdventOfCode2024/Day3/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+void Part1(string s)
+{
+    var regex = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled);
+
+    var total = 0;
+    var matches = regex.Matches(s);
+    foreach (Match match in matches)
+    {
+        var x = int.Parse(match.Groups[1].Value);
+        var y = int.Parse(match.Groups[2].Value);
+        total += x * y;
+    }
+
+    Console.WriteLine("Part 1: " + total);
+}
+
+void Part2(string s)
+{
+    var regex = new Regex(@"do\(\)|don\'t\(\)|mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Compiled);
+
+    var total = 0;
+    var matches = regex.Matches(s);
+    var enabled = true;
+    foreach (Match match in matches)
+    {
+        if (match.Value == "do()")
+        {
+            enabled = true;
+        }
+        else if (match.Value == "don't()")
+        {
+            enabled = false;
+        }
+        else if (enabled)
+        {
+            var x = int.Parse(match.Groups[1].Value);
+            var y = int.Parse(match.Groups[2].Value);
+            total += x * y;
+        }
+    }
+
+    Console.WriteLine("Part 2: " + total);
+}
+
+var input = File.ReadAllText("input1.txt");
+Part1(input);
+Part2(input);
